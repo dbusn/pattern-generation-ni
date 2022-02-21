5c87e60ed68caabee2a0beca4f40c7c7613eba39
CocoTheOwner <sjoerdvdgoor@hotmail.com>
2022-02-21 17:28:12 +0100

    Code cleanup

--- generator.py
+++ generator.py
@@ -1,19 +1,18 @@
-import string
-import sys
-# from typing import Pattern
+from io import UnsupportedOperation
+import os, json, sys, string, random
 import numpy as np
-from scipy import signal
-import GifDisplay
-import reed_pattern_data
-import random
+import moviepy.editor
+import scipy.signal
 
 # type of E: 'set' which is a subset of (4,6), e in E, e[0] is '4', e[1] is '6'
 # type of D: duration in of the pattern (integer in ms)
 # type of G_f: frequency of wave type (integer)
 # type of W: string, denoting the wave type
 
+POSSIBLE_WAVEFORMS = ['sin', 'sawtooth', 'block', 'hanning', 'constant']
 
-def staticPattern(E, a, f, d, g_f, w):
+
+def staticPattern(E, a, f, d, g_f, w) -> list:
     """
     #TODO: add drawing somewhere
 
@@ -82,7 +81,7 @@ def staticPattern(E, a, f, d, g_f, w):
         phi = 0
         amplitude_list = []
         for i in range(len(x)):
-            amplitude_list.append((int) ((max_amp*signal.sawtooth(B * (x[i] - phi)) + max_amp)/2))
+            amplitude_list.append((int) ((max_amp*scipy.signal.sawtooth(B * (x[i] - phi)) + max_amp)/2))
 
         for timestep in range(discretization):
             for e in E:
@@ -131,93 +130,103 @@ def staticPattern(E, a, f, d, g_f, w):
 
     return array
 
+"""
+:param name: (str) name of the pattern
+:param grid_width: (int) width of the grid
+:param grid_height: (int) height of the grid
+:param actuators_no: (tuple) -> (lower, upper) bound on amounts of actuators enabled. 
+:param amplitudes: (list) list of amplitudes
+:param frequencies: (list) list of frequencies
+:param modulations: (list) list of modulation frequencies
+:param durations: (list) list of durations
+:param waveforms: (list) waveform options, pick one or more from from ['sin', 'sawtooth', 'block', 'hanning', 'constant']
+:return: tuple of (name: str, pattern: list). Pattern representing the pattern in video form. pattern = pattern[time][x][y][extra].
+         note that pattern[][][][0] gives the frequency, and pattern[][][][1] gives the amplitude.
+"""
+def generateRandomPattern(
+    name: str,
+    grid_width: int = 4,
+    grid_height: int = 6,
+    actuators_no: tuple = (1, 8),
+    amplitudes: list = [100, 255],
+    frequencies: list = [69],
+    modulations: list = [8],
+    durations: list = [92, 392],
+    waveforms: list = POSSIBLE_WAVEFORMS) -> tuple:
 
-# ################ testing: #################
-# max_amp = 250
-# t = np.linspace(0, 1, 500)
-# plt.plot(t, (max_amp*signal.sawtooth(2 * np.pi * t) + max_amp)/2)
-# plt.show()
+    actuators = random.randint(actuators_no[0], actuators_no[1])
+    amplitude = random.choice(amplitudes)
+    frequency = random.choice(frequencies)
+    modulation = random.choice(modulations)
+    duration = random.choice(durations)
+    waveform = random.choice(waveforms)
+    waveform = waveform if waveform in POSSIBLE_WAVEFORMS else random.choice(POSSIBLE_WAVEFORMS)
 
-# print((max_amp*signal.sawtooth(2 * np.pi * 5 * t) + max_amp)/2)
+    enabled_actuators = [(random.randint(1,grid_height), random.randint(1,grid_width)) for _ in range (actuators)]
 
-
-def generatePatterns(phonemes: list) -> list:
-    gen_patterns = []
-
-    for p in phonemes:
-        # Subset of the grid a.k.a coords of actuators used by the pattern
-        e = p.coord_list
-        # Amplitude
-        # Low ~ 100, High ~ 250
-        a = 100
-        # a = 250
-        # Duration (total_time)
-        d = p.total_time
-        # Frequency
-        f = p.pho_freq
-        # Group frequency (modulation)
-        g_f = p.modulation
-        # Wave type
-        w = 'sin'
-        gen_patterns.append([str(p.phoneme), staticPattern(e, a, f, d, g_f, w)])
-
-        return gen_patterns
-
-
+    return (name, staticPattern(enabled_actuators, amplitude, frequency, duration, modulation, waveform))
 
 """
-:param max_actuators: (int) Minimum number of tectonic exciters in a pattern
-:param max_actuators: (int) Maximum number of tectonic exciters in a pattern
-:return: array: (array) an array representing the pattern in video form. array = array[time][x][y][extra].
-         note that array[][][][0] gives the frequency, and array[][][][1] gives the amplitude.
- """
-def generateRandomPattern(name: str, min_actuators: int, max_actuators: int) -> list:
-    # Temporary boundaries defined here:
-    gen_patterns = []
-    # Coordinate list
-    n_actuators = random.randint(min_actuators, max_actuators)
-    e = []
-    for actuator in range(n_actuators):
-        row = random.randint(1,6)
-        col = random.randint(1,4)
-        e.append([col, row])
+:param name: (str) name of the pattern
+:param grid: np.ndarray representing the pattern in video form. gird = grid[time][x][y][extra].
+         note that grid[][][][0] gives the frequency, and grid[][][][1] gives the amplitude.
+:param grid_width: (int) width of the grid
+:param grid_height: (int) height of the grid
+:param channel_red: (str) amplitude or frequency
+:param channel_green: (str) amplitude or frequency
+:param channel_blue: (str) amplitude or frequency
+"""
+def generateGIF(
+    name: string,
+    grid: np.ndarray,
+    grid_width: int = 4,
+    grid_height: int = 6,
+    channel_red: str = "amplitude",
+    channel_green: str = "amplitude",
+    channel_blue: str = "amplitude") -> None:
 
-    amplitudes = [100, 250]
-    a = random.choice(amplitudes)
+    grids = [[[[255, 255, 255] for _ in range(grid_width)] for _ in range(grid_height)] for _ in range(len(grid))]
 
-    modulations = [8, 30]
-    g_f = random.choice(modulations)
-
-    waveforms = ['sin', 'sawtooth', 'block', 'hanning']
-    # w = random.choice(waveforms)
-    w = 'sin'
-
-    freqs = [60, 300]
-    f = random.choice(freqs)
-
-    # For now, fixed duration of 392ms
-    durations = [92, 392]
-    # d = random.choice(durations)
-    d = 392
-    gen_patterns = [name, staticPattern(e, a, f, d, g_f, w)]
-
-    return gen_patterns
-
-
-
-def generateGIF(name: string, grid: np.ndarray):
-    grids = [[[[255, 255, 255] for _ in range(4)] for _ in range(6)] for _ in range(len(grid))]
-
-    for index, value in np.ndenumerate(grid):
+    for index, _ in np.ndenumerate(grid):
         # index:
         # (i, j, k, l) where:
         frame = grid[index[0]]
 
         for column_id, column in enumerate(frame):
             for row_id, row in enumerate(column):
-                freq = row[0]
-                amp = row[1]
-                grids[index[0]][row_id][column_id] = [255-amp, 255-amp, 255-amp]
+                if channel_red == "frequency" or channel_green == "frequency" or channel_blue == "frequency":
+                    raise UnsupportedOperation("frequency channel not supported yet")
+                amp = frame[column_id][row_id][1]
+                red = 255 - amp
+                green = 255 - amp
+                blue = 255 - amp
+                grids[index[0]][row_id][column_id] = [red, green, blue]
 
-    frames = GifDisplay.frames_from_lists(grids)
-    GifDisplay.save_frames_as_gif(frames, 'gifs', name)
+    save_frames_as_gif(frames_from_lists(grids), 'gifs', name)
+
+"""Saves a list of frames as a gif to the given output directory."""
+def save_frames_as_gif(frames: np.ndarray, output_dir: str, gif_name: str, fps: int = 10) -> None:
+
+    # If the output dir does not exist, create it
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Save the gif file
+    clip = moviepy.editor.ImageSequenceClip(list(frames), fps=fps)
+    clip.write_gif(os.path.join(output_dir, gif_name +  ".gif"), fps=fps)
+
+def frames_from_lists(lists: list) -> np.ndarray:
+    """Converts a list of arrays into a list of frames."""
+    return [np.array(_list, dtype=np.uint8) for _list in lists]
+
+if __name__ == "__main__":
+    iters = [iteration['iteration'] for iteration in json.loads(sys.argv[1])['pattern']]
+    grids = [[[[255, 255, 255] for _ in range(0,4)] for _ in range(0,6)] for _ in range(0,len(iters))]
+
+    for i, iteration in enumerate(iters):
+        col_coord = int(str(iteration[0]['coord'])[0])
+        row_coord = int(str(iteration[0]['coord'])[1])
+        amp = iteration[0]['amplitude']
+        grids[i][row_coord-1][col_coord-1] = [255-amp,255-amp, 255-amp]
+
+    save_frames_as_gif(frames_from_lists(grids), 'gifs', sys.argv[1].split('.')[0])
