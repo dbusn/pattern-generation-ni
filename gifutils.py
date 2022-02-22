1c23e25f9c4aae339b3b3eb9a8e93b1eab17c717
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2022-02-22 15:36:36 +0100

    Move core to pattern gen notebook

---
+++ gifutils.py
@@ -0,0 +1,15 @@
+import np, os, moviepy
+"""Saves a list of frames as a gif to the given output directory."""
+def save_frames_as_gif(frames: np.ndarray, output_dir: str, gif_name: str, fps: int = 10) -> None:
+
+    # If the output dir does not exist, create it
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Save the gif file
+    clip = moviepy.editor.ImageSequenceClip(list(frames), fps=fps)
+    clip.write_gif(os.path.join(output_dir, gif_name +  ".gif"), fps=fps)
+
+"""Converts a list of arrays into a list of frames."""
+def frames_from_lists(lists: list) -> np.ndarray:
+    return [np.array(_list, dtype=np.uint8) for _list in lists]
