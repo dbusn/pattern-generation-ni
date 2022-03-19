d10434dfd8e25f95b642f69c72b4878b5e5b0538
dbusn <dbustowski@protonmail.com>
2022-03-19 16:19:24 +0100

    Generated gifs for dynamic reed patterns

---
+++ json2gif/gifutils.py
@@ -0,0 +1,23 @@
+import numpy, os, moviepy.editor
+
+"""Saves a list of frames as a gif to the given output directory."""
+
+
+def save_frames_as_gif(
+    frames: numpy.ndarray, output_dir: str, gif_name: str, fps: int = 10
+) -> None:
+
+    # If the output dir does not exist, create it
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Save the gif file
+    clip = moviepy.editor.ImageSequenceClip(list(frames), fps=fps)
+    clip.write_gif(os.path.join(output_dir, gif_name + ".gif"), fps=fps)
+
+
+"""Converts a list of arrays into a list of frames."""
+
+
+def frames_from_lists(lists: list) -> numpy.ndarray:
+    return [numpy.array(_list, dtype=numpy.uint8) for _list in lists]
